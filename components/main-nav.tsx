b7b08b76e32be329b67e24d0705441586460638b
Raihan Adam <hallwack.id@gmail.com>
2023-01-28 13:52:09 +0700

    fix: fixing dropdown nav in mobile

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,18 +2,8 @@ import * as React from "react"
 import Link from "next/link"
 
 import { NavItem } from "@/types/nav"
-import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 interface MainNavProps {
   items?: NavItem[]
@@ -22,11 +12,8 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
-        <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
+      <Link href="/" className="items-center space-x-2 flex">
+        <Icons.logo className="h-32 w-32" />
       </Link>
       {items?.length ? (
         <nav className="hidden gap-6 md:flex">
@@ -47,37 +34,6 @@ export function MainNav({ items }: MainNavProps) {
           )}
         </nav>
       ) : null}
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            className="-ml-4 text-base hover:bg-transparent focus:ring-0 md:hidden"
-          >
-            <Icons.logo className="mr-2 h-4 w-4" />{" "}
-            <span className="font-bold">Menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          align="start"
-          sideOffset={24}
-          className="w-[300px] overflow-scroll"
-        >
-          <DropdownMenuLabel>
-            <Link href="/" className="flex items-center">
-              <Icons.logo className="mr-2 h-4 w-4" /> {siteConfig.name}
-            </Link>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {items?.map(
-            (item, index) =>
-              item.href && (
-                <DropdownMenuItem key={index} asChild>
-                  <Link href={item.href}>{item.title}</Link>
-                </DropdownMenuItem>
-              )
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
     </div>
   )
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,24 @@
-import { siteConfig } from "@/config/site"
+import * as React from "react"
+import Link from "next/link"
 
+import { siteConfig } from "@/config/site"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 export function SiteHeader() {
   return (
@@ -14,6 +28,36 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <div className="flex w-full max-w-sm items-center space-x-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="hover:bg-transparent focus:ring-0 md:hidden"
+                  >
+                    <Icons.menu className="mr-2 h-6 w-6" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  align="start"
+                  sideOffset={6}
+                  className="w-[300px] mr-4 2xl:mr-0 overflow-scroll"
+                >
+                  <DropdownMenuLabel>
+                    <Link href="/" className="flex items-center">
+                      <Icons.logo className="mr-2 h-6 w-6" /> {siteConfig.name}
+                    </Link>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  {siteConfig.mainNav?.map(
+                    (item, index) =>
+                      item.href && (
+                        <DropdownMenuItem key={index} asChild>
+                          <Link href={item.href}>{item.title}</Link>
+                        </DropdownMenuItem>
+                      )
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
               <Popover>
                 <PopoverTrigger asChild>
                   <Button type="button" variant="ghost" size="sm">
